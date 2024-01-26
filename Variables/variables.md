090017a04096a40a5c07ebf002e3299e12249df3
yamiranea <yamilarenea@gmail.com>
2024-01-26 13:14:05 +0100

    completing variables tasks

--- Variables/variables.md
+++ Variables/variables.md
@@ -28,3 +28,39 @@ $null = null;
 ### PART 2
 
 - What type of data are the following variables:
+
+```php
+Easy level
+$nombre = "Luna"; //string
+$apellido = "Lovegood"; //string
+$edad = 42; //integer
+$Ravenclaw = true; //boolean
+
+Medium level
+$average = (8 + 9.5 + 9 + 10 + 8) / 5; //float = 8.9
+$full_name = $name . " " . $last_name; // string = Luna Lovegood
+$test_doing = (bool) 1; // boolean = true
+
+Advanced level
+$questions_amount = 5 + "5"; // int = 10
+$answers_amount = "5" + 5; // int = 10
+$max_average = $answers_amount / 1.0; // float = 10.0
+$nargles = 3 . "5 nargles"; // string = 35 nargles
+```
+
+### PART 3
+
+- Assuming these variables, what is the result according to the truth tables?
+
+```php
+$is_a_big_cat = true;
+$he_likes_to_eat = true;
+$he_can_fly = false;
+$he_has_2_legs = false;
+
+$is_a_big_cat && $he_likes_to_eat; //true
+$is_a_big_cat || $he_can_fly; //false
+$he_can_fly || $he_has_2_legs; //false
+!$he_likes_to_eat; //true
+!$he_likes_to_eat || $is_a_big_cat; //true
+```
--- Variables/variables.php
+++ Variables/variables.php
@@ -7,7 +7,7 @@ PART 1 - TASKS:
 -Declare a variable for each data type and assign a value to it
 -Display the value of that variable in the console and on the screen. Boolean (printing true or false), an array, and a null value (printing NULL).
 */
-print_r("<h4>TASK 1: Declare a variable for each data type and assign a value to it. Display the value of that variable in the console and on the screen. Boolean (printing true or false), an array, and a null value (printing NULL).</h4> \n");
+print_r("<h4>PART 1: Declare a variable for each data type and assign a value to it. Display the value of that variable in the console and on the screen. Boolean (printing true or false), an array, and a null value (printing NULL).</h4> \n");
 
 
 // String
@@ -39,25 +39,69 @@ echo "Declaring a variable type: " . gettype($null) . "\n" . "<br>";
 /* 
 PART 2: What type of data are the following variables
 */
-print_r("<h4>TASK 2: What type of data are the following variables?</h4> \n");
+print_r("<h4>PART 2: What type of data are the following variables?</h4> \n");
 
-// Easy
-echo "<b>Easy \n </b><br>";
+// Easy level
+echo "Easy \n ";
 
 $name = "Luna";
+var_dump($name);
+echo "\n ";
 $last_name = "Lovegood";
+var_dump($last_name);
+echo "\n ";
 $age = 42;
-$Ravenclaw = true;
-
-$nombre = "Luna";
-var_dump($nombre);
-echo "\n <br>";
-$apellido = "Lovegood";
-var_dump($apellido);
-echo "\n <br>";
-$edad = 42;
-var_dump($edad);
-echo "\n <br>";
+var_dump($age);
+echo "\n ";
 $Ravenclaw = true;
 var_dump($Ravenclaw);
-echo "\n <br><br>";
+echo "\n ";
+
+// Medium level
+echo "Medium level \n ";
+
+$average = (8 + 9.5 + 9 + 10 + 8) / 5;
+var_dump($average);
+echo "\n";
+$full_name = $name . " " . $last_name;
+var_dump($full_name);
+echo "\n";
+$test_doing = (bool) 1;
+var_dump($test_doing);
+echo "\n";
+
+// Advanced level
+echo "Advanced level \n ";
+
+$questions_amount = 5 + "5";
+var_dump($questions_amount);
+echo "\n ";
+$answers_amount = "5" + 5;
+var_dump($answers_amount);
+echo "\n ";
+$max_average = $answers_amount / 1.0;
+var_dump($max_average);
+echo "\n ";
+$nargles = 3 . "5 nargles";
+var_dump($nargles);
+echo "\n ";
+
+/* 
+PART 3 - PART: assuming these variables, what is the result according to the truth tables?
+*/
+print_r("<h4>PART 3 - TASKS: assuming these variables, what is the result according to the truth tables?.</h4> \n");
+$is_a_big_cat = true;
+$he_likes_to_eat = true;
+$he_can_fly = false;
+$he_has_2_legs = false;
+
+var_dump($is_a_big_cat && $he_likes_to_eat);
+echo "\n <br>";
+var_dump($is_a_big_cat || $he_can_fly);
+echo "\n <br>";
+var_dump($he_can_fly || $he_has_2_legs);
+echo "\n <br>";
+var_dump(!$he_likes_to_eat);
+echo "\n <br>";
+var_dump(!$he_likes_to_eat || $is_a_big_cat);
+echo "\n <br>";
